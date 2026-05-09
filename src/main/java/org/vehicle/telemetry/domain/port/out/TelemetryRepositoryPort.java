3933a90c8ff964f2b3d649beaf270fbb42646fd6
GabrielLeis <wPnw54@QpfjGFGe>
2026-05-09 10:36:46 +0200

    fix(TelemetryRepositoryPort): Fixed Wrong Class Name

--- src/main/java/org/vehicle/telemetry/domain/port/out/TelemetryRepositoryPort.java
+++ src/main/java/org/vehicle/telemetry/domain/port/out/TelemetryRepositoryPort.java
@@ -2,7 +2,7 @@ package org.vehicle.telemetry.domain.port.out;
 
 import org.vehicle.telemetry.domain.model.VehicleTelemetry;
 
-public interface TemeletryRepositoryPort {
+public interface TelemetryRepositoryPort {
 
     public void save(VehicleTelemetry vehicle);
 }
